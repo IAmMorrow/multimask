91cbad733ccb2441b8c2f585221eb90f7f26f56c
huiqi <huiqi.zheng@ledger.fr>
2021-06-22 20:48:48 +0200

    websocket connection methods added

--- src/ui/wallet.tsx
+++ src/ui/wallet.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import React from "react";
 import { Logo } from "./icons/logo";
+import { useEffect } from 'react';
 
 const TopBar = styled.div`
     width: 100%;
@@ -16,7 +17,45 @@ const LogoContainer = styled.div`
     padding: 16px;
 `
 
+
+let websocket = null;
+function createWebSocketConnection(host) {
+    if('WebSocket' in window) {
+        websocket = new WebSocket(host);
+
+        websocket.onopen = function() {
+            console.log("websocket connected");
+        };
+
+        websocket.onmessage = function (event) {
+            var received_msg = JSON.parse(event.data);
+            var notificationOptions = {
+                type: "basic",
+                title: received_msg.title,
+                message: received_msg.message,
+                iconUrl: "extension-icon.png"
+            }
+            chrome.notifications.create("", notificationOptions);
+        };
+
+        websocket.onclose = function() {
+            alert("==== web socket closed ======");
+        };
+    }
+}
+
+function onConnect() {
+    createWebSocketConnection("ws://127.0.0.1:8084");
+}
+
+
 export function Wallet() {
+    useEffect(() => {
+        console.log("connecting websocket");
+        if (!websocket){
+            onConnect();
+        }
+      }, []);
     return (
         <WalletContainer>
             <TopBar>
