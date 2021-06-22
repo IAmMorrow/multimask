b71b1bf18285d0a91557fab2f9b758c6ce6bc215
Hugo Morosini <hugo.morosini@gmail.com>
2021-06-22 17:31:10 +0200

    css works

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,4 +1,62 @@
+import { constants } from "buffer";
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log("Background got a message!")
     sendResponse({})
 })
+
+let websocket = null;
+
+function onConnect() {
+    if (!websocket) {
+        throw new Error("connect error: already connected");
+    }
+    createWebSocketConnection("127.0.0.1:8084");
+}
+
+function onSend(message: string) {
+    if (!websocket) {
+        throw new Error("send error: already connected");
+    }
+    websocket.send(message);
+}
+
+chrome.extension.onRequest.addListener(
+    async function(request, sender, sendResponse) {
+        switch (request.method) {
+            case "connect":
+                onConnect();
+            case "send":
+                onSend(request.params)
+                break;
+            default:
+                break;
+        }
+    }
+);
+
+function createWebSocketConnection(host) {
+
+    if('WebSocket' in window) {
+        websocket = new WebSocket(host);
+
+        websocket.onopen = function() {
+
+        };
+
+        websocket.onmessage = function (event) {
+            var received_msg = JSON.parse(event.data);
+            var notificationOptions = {
+                type: "basic",
+                title: received_msg.title,
+                message: received_msg.message,
+                iconUrl: "extension-icon.png"
+            }
+            chrome.notifications.create("", notificationOptions);
+        };
+
+        websocket.onclose = function() {
+            alert("==== web socket closed ======");
+        };
+    }
+}
--- src/ui/popup.tsx
+++ src/ui/popup.tsx
@@ -1,14 +1,24 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
+import styled from "styled-components"
+import { Reset } from "styled-reset"
 
 import "../styles/popup.css"
+
+const Container = styled.div`
+    width: 360px;
+    height: 600px;
+    background-color: black;
+`
 
 class Hello extends React.Component {
     render() {
         return (
-            <div className="popup-padded">
+            <Container>
+                <Reset />
+                test
                 <h1>{ chrome.i18n.getMessage("l10nHello") }</h1>
-            </div>
+            </Container>
         )
     }
 }
