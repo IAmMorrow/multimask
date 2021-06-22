428b801728375e9a18474af391f760b003fbb77d
Hugo Morosini <hugo.morosini@gmail.com>
2021-06-22 20:45:49 +0200

    added currency selector

---
+++ src/ui/components/CurrencySelect.tsx
@@ -0,0 +1,133 @@
+import Select, { components, OptionTypeBase } from "react-select";
+import React, { useMemo, useCallback } from "react";
+import Image from 'next/image'
+import styled, { useTheme, DefaultTheme } from "styled-components";
+import Color from "color";
+import { Currency } from "../../types/types";
+import { defaultTheme } from "../../styles/theme";
+
+const IconContainer = styled.div`
+    margin-right: 0.4em;
+    flex-shrink: 0;
+`
+
+const AccountDetails = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    flex-grow: 1;
+`
+
+const AccountName = styled.span`
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-weight: bold;
+`
+
+const AccountAddress = styled.span`
+    overflow: hidden;
+    text-overflow: ellipsis;
+    opacity: 0.5;
+`
+
+const AccountIcon = () => (
+    <IconContainer>
+        <Image src="/icons/ethereum.svg" width={24} height={24} />
+    </IconContainer>
+);
+
+const AccountOption: typeof components.Option = ({ children, data, ...rest }) => (
+    <components.Option data={data} {...rest}>
+        <AccountIcon />
+        <AccountDetails>
+            <AccountName>{children}</AccountName>
+            <AccountAddress>{data.value}</AccountAddress>
+        </AccountDetails>
+    </components.Option>
+);
+
+const AccountSummary: typeof components.SingleValue = ({ children, ...rest }) =>
+    <components.SingleValue {...rest}>
+        <AccountIcon />
+        <AccountName>{children}</AccountName>
+    </components.SingleValue>
+
+const getSelectStyles = (theme: DefaultTheme) => ({
+    control: (provided: any) => ({
+        ...provided,
+        width: 200,
+        backgroundColor: "transparent",
+    }),
+    singleValue: (provided: any) => ({
+        ...provided,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        fontSize: 12,
+        color: theme.colors.text,
+    }),
+    indicatorsContainer: (provided: any) => ({
+        ...provided,
+        color: "red",
+    }),
+    menu: (provided: any) => ({
+        ...provided,
+        backgroundColor: theme.colors.background,
+    }),
+    option: (provided: any, { isFocused, isSelected }: { isFocused: boolean, isSelected: boolean }) => ({
+        ...provided,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        fontSize: 12,
+        color: isSelected ? "#fff" : theme.colors.text,
+        backgroundColor: isSelected
+            ? new Color(theme.colors.primary).alpha(isFocused ? 0.8 : 1).string()
+            : isFocused
+                ? new Color(theme.colors.primary).alpha(0.1).string()
+                : "transparent",
+    })
+});
+
+type CurrencySelectorProps = {
+    currencies: Currency[],
+    onCurrencyChange: (currency: Currency | undefined) => void,
+    selectedCurrency: Currency | undefined,
+};
+
+function fromCurrencyToOption(currency: Currency): OptionTypeBase {
+    return {
+        label: currency.name,
+        value: currency.id,
+        data: {
+
+        },
+    }
+}
+
+export function CurrencySelect({ currencies, onCurrencyChange, selectedCurrency }: CurrencySelectorProps) {
+    const theme = defaultTheme;
+    const options = useMemo(() => currencies.map(currency => fromCurrencyToOption(currency)), [currencies]);
+    const value = useMemo(() => selectedCurrency ? fromCurrencyToOption(selectedCurrency) : undefined, [selectedCurrency]);
+
+    const styles = useMemo(() => getSelectStyles(theme), [theme]);
+
+    const handleOnChange = useCallback((option: OptionTypeBase | null) => {
+        const newSelectedCurrency = option ? currencies.find(currency => currency.id === option.value) : undefined;
+        onCurrencyChange(newSelectedCurrency);
+    }, [currencies, onCurrencyChange])
+
+    return (
+        <div>
+        <Select
+            instanceId="account"
+            options={options}
+            styles={styles}
+            components={{ SingleValue: AccountSummary, Option: AccountOption}}
+            onChange={handleOnChange}
+            value={value}
+            isSearchable={false}
+        />
+        </div>
+    )
+}
--- src/ui/wallet.tsx
+++ src/ui/wallet.tsx
@@ -1,10 +1,16 @@
 import styled from "styled-components";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Logo } from "./icons/logo";
+import { CurrencySelect } from "./components/CurrencySelect";
 
 const TopBar = styled.div`
     width: 100%;
     border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 16px;
+    box-sizing: border-box;
 `;
 
 const WalletContainer = styled.div`
@@ -13,16 +19,23 @@ const WalletContainer = styled.div`
 `;
 
 const LogoContainer = styled.div`
-    padding: 16px;
+
 `
 
 export function Wallet() {
+    const [ selectedCurrency, setSelectedCurrency ] = useState(undefined);
+
+    const onCurrencyChange = useCallback((currency) => {
+        setSelectedCurrency(selectedCurrency);
+    }, []);
+    
     return (
         <WalletContainer>
             <TopBar>
                 <LogoContainer>
                     <Logo height={36} width={41} />
                 </LogoContainer>
+                <CurrencySelect currencies={[]} onCurrencyChange={onCurrencyChange} selectedCurrency={selectedCurrency} />
             </TopBar>
         </WalletContainer>
     );
